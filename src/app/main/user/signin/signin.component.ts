75fe1fbae62075a097cd4bf5bf0016a1670b45b6
rafatuba <rafaolly1@gmail.com>
2024-04-05 19:10:59 -0400

    atualizado front

--- src/app/main/user/signin/signin.component.ts
+++ src/app/main/user/signin/signin.component.ts
@@ -1,24 +1,33 @@
 import { NewUser } from './new-user';
 import { Component, OnInit } from '@angular/core';
-import {  FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { SigninService } from './signin.service';
 import { UserExistsService } from './user-exists.service';
 import { MensageService } from '../../../components/mensage/mensage.service';
-import { EqualsPasswordValidator, NumberValidator, SpecialCharValidator, UperCharValidator, UserEqualPasswordValidator } from './password-validator';
+import {
+  EqualsPasswordValidator,
+  NumberValidator,
+  SpecialCharValidator,
+  UperCharValidator,
+  UserEqualPasswordValidator,
+} from './password-validator';
 import { MensageComponent } from '../../../components/mensage/mensage.component';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-
 @Component({
   standalone: true,
-  imports: [ReactiveFormsModule,MensageComponent,RouterLink,CommonModule],
+  imports: [ReactiveFormsModule, MensageComponent, RouterLink, CommonModule],
   providers: [FormBuilder],
   selector: 'app-novo-usuario',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss'],
-
 })
 export class SigninComponent implements OnInit {
   newUserForm!: FormGroup;
@@ -29,19 +38,29 @@ export class SigninComponent implements OnInit {
     private formBuilder: FormBuilder,
     private signinService: SigninService,
     private router: Router
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.newUserForm = this.formBuilder.group(
       {
-        userName: [
-          '', [Validators.required],
-        ],
+        userName: ['', [Validators.required]],
         email: ['', [Validators.required, Validators.email]],
-        password: ['',[Validators.required,Validators.minLength(6),Validators.maxLength(20),
-        ,SpecialCharValidator.bind(this),UperCharValidator.bind(this),NumberValidator.bind(this)]],
-        repassword: ['',[Validators.required, EqualsPasswordValidator.bind(this)]]
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20),
+            ,
+            SpecialCharValidator.bind(this),
+            UperCharValidator.bind(this),
+            NumberValidator.bind(this),
+          ],
+        ],
+        repassword: [
+          '',
+          [Validators.required, EqualsPasswordValidator.bind(this)],
+        ],
       },
       {
         validators: [UserEqualPasswordValidator],
@@ -50,7 +69,7 @@ export class SigninComponent implements OnInit {
   }
 
   signin() {
-    console.log(this.newUserForm.valid)
+    console.log(this.newUserForm.valid);
     if (this.newUserForm.valid) {
       const novoUsuario = this.newUserForm.getRawValue() as NewUser;
       this.signinService.SigninNewUser(novoUsuario).subscribe(
@@ -59,14 +78,14 @@ export class SigninComponent implements OnInit {
         },
         (e) => {
           this.loading = false;
-          var erros = e.error.reasons
+          var erros = e.error.reasons;
 
-          switch(erros[0].message){
-            case "Failed : DuplicateUserName":
-                this.mensageService.ErrorMensage("Usúario já cadastrado");
+          switch (erros[0].message) {
+            case 'Failed : DuplicateUserName':
+              this.mensageService.ErrorMensage('Usúario já cadastrado');
               break;
-              default:
-                this.mensageService.SuccessMensage(erros[0].message);
+            default:
+              this.mensageService.SuccessMensage(erros[0].message);
               break;
           }
         }
